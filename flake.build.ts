eb6782fafdf412757d8edf5ec4b453d07f0f4e26
hasundue <hasundue@gmail.com>
2023-11-20 23:03:39 +0900

    chore: format code

--- flake.build.ts
+++ flake.build.ts
@@ -16,8 +16,7 @@ ${PLACEHOLDER}
     formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixpkgs-fmt;
     plugins = inputs;
   };
-}`
-;
+}`;
 
 async function generateFlake(
   plugins: ClosedGroup,
